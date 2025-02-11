1c2d7b17fd23c592e8d1b536ddf662f83cd879c2
GiantLionTurtle <charle.tanguay@gmail.com>
2025-02-11 12:54:03 -0500

    multiple constructors methods

--- klippy/kinematics/convergent_delta.py
+++ klippy/kinematics/convergent_delta.py
@@ -30,45 +30,71 @@ class Actuator:
 
 
 class ConvergentDeltaKinematics:
-    def __init__(self, toolhead, config):
+    def __init__(self):
+
+        self.rails = []
+
+        # Read config
+        self.max_velocity = 0.0
+        self.max_accel = 0.0
+        
+        self.max_z_velocity = 0.0
+        self.max_z_accel = 0.0
+
+        self.actuators = []
+
+        self.arm_lengths = arm_lengths = []
+        self.arm2 = []
+
+        self.need_home = True
+        self.home_position = None
+
+    @staticmethod
+    def make_for_klipper(toolhead, config):
+        out = ConvergentDeltaKinematics()
+
         stepper_configs = [config.getsection('stepper_' + a) for a in 'abc']
         rail_a = stepper.PrinterRail(stepper_configs[0], need_position_minmax=True, units_in_radians=False)
         a_endstop = rail_a.get_homing_info().position_endstop
         rail_b = stepper.PrinterRail(stepper_configs[1], need_position_minmax=True, units_in_radians=False, default_position_endstop=a_endstop)
         rail_c = stepper.PrinterRail(stepper_configs[2], need_position_minmax=True, units_in_radians=False, default_position_endstop=a_endstop)
 
-        self.rails = [rail_a, rail_b, rail_c]
+        out.rails = [rail_a, rail_b, rail_c]
 
         # Read config
-        self.max_velocity, self.max_accel = toolhead.get_max_velocity()
-        self.max_z_velocity = config.getfloat('max_z_velocity', self.max_velocity, above=0., maxval=self.max_velocity)
-        self.max_z_velocity = config.getfloat('max_z_accel', self.max_accel, above=0., maxval=self.max_accel)
+        out.max_velocity, out.max_accel = toolhead.get_max_velocity()
+        out.max_z_velocity = config.getfloat('max_z_velocity', out.max_velocity, above=0., maxval=out.max_velocity)
+        out.max_z_accel = config.getfloat('max_z_accel', out.max_accel, above=0., maxval=out.max_accel)
 
-        self.actuators = [Actuator( [sconfig.getfloat('min_x'), sconfig.getfloat('min_y'), sconfig.getfloat('min_z')],
+        out.actuators = [Actuator( [sconfig.getfloat('min_x'), sconfig.getfloat('min_y'), sconfig.getfloat('min_z')],
                                     [sconfig.getfloat('max_x'), sconfig.getfloat('max_y'), sconfig.getfloat('max_z')])
                                     for sconfig in stepper_configs]
 
         arm_length_a = stepper_configs[0].getfloat('arm_length', above=0)
-        self.arm_lengths = arm_lengths = [sconfig.getfloat('arm_length', arm_length_a, above=0)
+        out.arm_lengths = arm_lengths = [sconfig.getfloat('arm_length', arm_length_a, above=0)
                                             for sconfig in stepper_configs]
-        self.arm2 = [arm**2 for arm in arm_lengths]
+        out.arm2 = [arm**2 for arm in arm_lengths]
 
-        self.need_home = True
-        self.home_position = tuple(self._actuators_to_cartesian([0.0, 0.0, 0.0]))
+        out.need_home = True
+        out.home_position = tuple(out._actuators_to_cartesian([0.0, 0.0, 0.0]))
         
 
-        for rail, arm2, act in zip(self.rails, self.arm2, self.actuators):
+        for rail, arm2, act in zip(out.rails, out.arm2, out.actuators):
             rail.setup_itersolve('convergent_delta_stepper_alloc', arm2, 
                 act.start[0], act.start[1], act.start[2], act.end[0], act.end[1], act.end[2])
-        for s in self.get_steppers():
+        for s in out.get_steppers():
             s.set_trapq(toolhead.get_trapq())
             toolhead.register_step_generator(s.generate_steps)
 
-        self.set_position([0.0, 0.0, 0.0], "")
+        out.set_position([0.0, 0.0, 0.0], "")
+        return out
 
-    def make_headless(self, actuators, arm2):
-        self.actuators = actuators
-        self.arm2 = [arm2, arm2, arm2]
+    @staticmethod
+    def make_headless(actuators, arm2):
+        out = ConvergentDeltaKinematics()
+        out.actuators = actuators
+        out.arm2 = [arm2, arm2, arm2]
+        return out
 
     def get_steppers(self):
         return [s for rail in self.rails for s in rail.get_steppers()]
@@ -108,4 +134,4 @@ class ConvergentDeltaKinematics:
         }
 
 def load_kinematics(toolhead, config):
-    return ConvergentDeltaKinematics(toolhead, config)
+    return ConvergentDeltaKinematics.make_for_klipper(toolhead, config)
